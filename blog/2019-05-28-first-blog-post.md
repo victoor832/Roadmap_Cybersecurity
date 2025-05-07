e6753c316dd77b08abaa5d0f169169697a2ee56f
vicana-r <victorca380@gmail.com>
2025-05-07 16:32:30 +0200

    Fix image size

--- blog/2019-05-28-first-blog-post.md
+++ blog/2019-05-28-first-blog-post.md
@@ -10,3 +10,8 @@ Lorem ipsum dolor sit amet...
 <!-- truncate -->
 
 ...consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet
+
+
+<!--<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vR-NIIkeHEvSreLOH50l-j06MTrqiC14my5bAzd9d-8ln6ToCo1LscrRFmuPxjYS5JhWTZuWfudQkyK/pubembed?start=false&loop=false&delayms=3000"
+frameborder="0" width="800" height="569" allowfullscreen="true"
+mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>-->
--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ function HomepageHeader() {
             <p className="hero__subtitle">Recursos y guías avanzadas para profesionales de la ciberseguridad</p>
             <div className={styles.buttons}>
               <Link
-                to="/blog"
+                to="/docs"
                 style={buttonStyleStart}
                 onMouseOver={() => setHovered(true)}
                 onMouseOut={() => setHovered(false)}
@@ -93,7 +93,7 @@ function HomepageHeader() {
             </div>
           </div>
           <div className={styles.heroImage}>
-            <Image img={require('../../static/img/banner.png')} />
+            <Image img={require('/img/banner.webp')} />
           </div>
         </div>
       </div>
@@ -134,25 +134,25 @@ function SecurityTopics() {
       title: 'Análisis de malware',
       icon: '🔬',
       description: 'Técnicas avanzadas para analizar y comprender código malicioso',
-      link: '/docs/malware-analysis'
+      //link: '/docs/malware-analysis'
     },
     {
       title: 'Pentesting',
       icon: '🛡️',
       description: 'Metodologías, herramientas y procedimientos para pruebas de penetración',
-      link: '/docs/pentesting'
+      //link: '/docs/pentesting'
     },
     {
       title: 'Respuesta a incidentes',
       icon: '🚨',
       description: 'Protocolos y mejores prácticas para gestionar brechas de seguridad',
-      link: '/docs/incident-response'
+      //link: '/docs/incident-response'
     },
     {
       title: 'Desarrollo de herramientas',
       icon: '👨🏻‍💻',
       description: 'Creación de scripts y herramientas personalizadas para la ciberseguridad',
-      link: '/docs/cloud-security'
+      //link: '/docs/cloud-security'
     },
   ];
 
@@ -305,16 +305,10 @@ function CommunitySection() {
             <h2>Únete a nuestra comunidad</h2>
             <p>Conecta con profesionales de la ciberseguridad, haz preguntas, comparte conocimientos y mantente al día con las últimas amenazas y defensas.</p>
             <div className={styles.communityButtons}>
-{/*               <a href="https://github.com/facebook/docusaurus" className="button button--secondary" target="_blank" rel="noopener noreferrer">
-                GitHub
-              </a> */}
               <button className={styles.github}>
                 <svg className={styles.githubIcon} viewBox="0 0 496 512" height="1.4em" xmlns="http://www.w3.org/2000/svg"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>
                 <span className={styles.githubtext}>Github</span>
               </button>
-{/*               <a href="https://discordapp.com/invite/docusaurus" className="button button--outline button--secondary" target="_blank" rel="noopener noreferrer" id='buttondarker'>
-                Discord
-              </a> */}
               <button className={styles.discord}>
                 <svg className={styles.discordIcon} viewBox="0 0 496 512" height="2em" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid" fill="#00000" stroke="#00000">
                   <g id="SVGRepo_bgCarrier" stroke-width="2"/>
@@ -326,7 +320,7 @@ function CommunitySection() {
             </div>
           </div>
           <div className={styles.communityImage}>
-            <img src="/img/community.svg" alt="Comunidad de ciberseguridad" />
+            <img src="/img/community.webp" alt="Comunidad de ciberseguridad" />
           </div>
         </div>
       </div>
